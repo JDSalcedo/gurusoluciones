972814f042726aaf8642fb3ee02c5c8bf01c73f3
Juan D. Salcedo Salazar <jsalcedo@minsa.gob.pe>
2023-07-31 09:51:35 -0500

    [ADD] Modulo Guru Academy

---
+++ guru_academy/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Guru Academy',
+    'version' : '1.0',
+    'summary': 'Academia Gurú',
+    'sequence': 10,
+    'description': """
+Academia Gurú
+====================
+Módulo para registrar las capacitaciones/cursos dictados en Guru Soluciones.
+    """,
+    'category': 'Academy/eLearning',
+    'website': 'https://www.gurusoluciones.com',
+    'depends' : ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
