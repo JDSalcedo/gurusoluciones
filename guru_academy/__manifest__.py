76dc63726fcb16d424204feec318e0d9797b724b
Gabriela Carbajales <gabriela.carbajales@gmail.com>
2023-08-11 13:34:47 -0300

    [ADD] Clases nuevas Academia y sede

--- guru_academy/__manifest__.py
+++ guru_academy/__manifest__.py
@@ -1,6 +1,7 @@
 {
-    'name' : 'Guru Academy',
-    'version' : '1.0',
+    'name': 'Guru Academy',
+    'version': '1.0',
+    'author': 'Gabi - gabriela.cabajales@gmail.com',
     'summary': 'Academia Gurú',
     'sequence': 10,
     'description': """
@@ -10,10 +11,14 @@ Módulo para registrar las capacitaciones/cursos dictados en Guru Soluciones.
     """,
     'category': 'Academy/eLearning',
     'website': 'https://www.gurusoluciones.com',
-    'depends' : ['base'],
+    'depends': ['base'],
     'data': [
+        'security/ir.model.access.csv',
+        'views/guru_academy_menus.xml',
+        'views/guru_academy_views.xml',
     ],
     'installable': True,
     'application': True,
     'auto_install': False,
+    'license': 'LGPL-3',
 }
