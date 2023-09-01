64d8aa666290bfd0ba953fa06d08643b55005453
Gabriela Carbajales <gabriela.carbajales@gmail.com>
2023-09-01 11:07:48 -0300

    [ADD] Módulo guru_airlines

---
+++ guru_airline/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Guru Airline',
+    'version': '1.0',
+    'author': 'Gabi - gabriela.cabajales@gmail.com',
+    'summary': 'Tracking de vuelos Gurú',
+    'sequence': 10,
+    'description': """
+Guru Airline
+============
+Módulo para realizar tracking de vuelos de la empresa Guru Soluciones.
+    """,
+    'category': 'Tools',
+    'website': 'https://www.gurusoluciones.com',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
