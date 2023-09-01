f91421de9d0f2abe9fc4294560405e5a9f83ccf4
Olmedo Javier <olmedoja@yellargentina.com>
2023-09-01 11:07:27 -0300

    Version Inicial de Guru Airline

---
+++ guru_airline/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Guru Airline',
+    'version': '1.0',
+    'author': 'Juan Salcedo - salcedo.salazar@gmail.com',
+    'summary': 'Tracking de vuelos',
+    'sequence': 10,
+    'description': """
+Guru Airline
+====================
+Módulo para realizar tracking de vuelos de la empresa Guru Soluciones.
+    """,
+    'category': 'Tools',
+    'website': 'https://www.gurusoluciones.com',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
