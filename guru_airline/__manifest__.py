8e5cf225b702c8158b0c7f201a6a99a913f1a236
pomaresj <jose.pomares@softdelnoa.com.ar>
2023-09-01 11:29:21 -0300

    [ADD] Nuevo módulo Guru Airline

---
+++ guru_airline/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Guru Airline',
+    'version': '1.0',
+    'author': 'José Pomares - jose.pomares@softdelnoa.com.ar',
+    'summary': 'Traking de vuelos',
+    'sequence': 10,
+    'description': """
+Guru Airline
+============
+Módulo para realizar tracking de vuelos de la empresa Guru Soluciones.
+    """,
+    'category': 'Tools',
+    'website': 'https://www.gurusoluciones.com',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
