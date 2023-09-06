38595a880a7f6ccd5044f03a76d93bd827306a1e
Gabriela Carbajales <gabriela.carbajales@gmail.com>
2023-09-06 10:22:03 -0300

    [ADD] clase Airline y Views

---
+++ guru_airline/models/airline.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+class Airline(models.Model):
+    _name = 'guru.airline'  # table: guru_airline
+    _description = 'Tabla para registro de Aerolíneas'
+
+    name = fields.Char(string='Nombre', required=True)
+    country_id = fields.Many2one(comodel_name='res.country', string='País', required=True)
+    street = fields.Char(string='Dirección')
+    zip = fields.Char(String='Código Postal', size=7)
+    phone = fields.Char(string='Teléfono')
